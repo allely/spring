7d4e4391050f5745ab1c0223ee2557a5a4d58ba0
allely <ggrocky49@gmail.com>
2023-12-29 18:07:43 +0900

    23-12-29

---
+++ 4.자바보강/datetimeex/src/exam01/Ex01.java
@@ -0,0 +1,25 @@
+package exam01;
+
+import java.util.Calendar;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+//        System.out.println(cal);  // 그레고리력. 지역에 맞춰 나온다.
+        showDate(cal);
+
+        cal.set(Calendar.DAY_OF_MONTH, 10);
+        showDate(cal);
+
+        cal.add(Calendar.DAY_OF_MONTH, 100);    // 100일 뒤
+        showDate(cal);
+    }
+
+    private static void showDate(Calendar cal) {
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);    // 0 ~ 11
+        int date = cal.get(Calendar.DAY_OF_MONTH);
+
+        System.out.printf("year=%d, month=%d, date=%d%n", year, month + 1, date);
+    }
+}
